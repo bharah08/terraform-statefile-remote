d06bd48eb5870d7863f8c1d0d27a162b2f071b22
bharah08 <111727649+bharah08@users.noreply.github.com>
2023-10-09 16:17:50 +0530

    Update terraform-remote-s3bucket.tf

--- terraform-remote-s3bucket.tf
+++ terraform-remote-s3bucket.tf
@@ -1,8 +1,23 @@
 
+
+provider "aws" {
+   region     = "ap-south-1"
+}
+
+resource "aws_dynamodb_table" "state_locking" {
+  hash_key = "LockID"
+  name     = "dynamodb-test"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+  billing_mode = "PAY_PER_REQUEST"
+}
+
 # Configure the S3 backend for storing Terraform state
 terraform {
   backend "s3" {
-    bucket         = "hellodevops0812222"
+    bucket         = "harddevops081292"
     key            = "terraform.tfstate"
     region         = "ap-south-1" # Change to your desired region
     encrypt        = true
