a4dc6b6a00b1b389a74373502489ad6c509f9b05
bharah08 <111727649+bharah08@users.noreply.github.com>
2023-10-09 19:50:59 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "s3remote0812" # change this
+    bucket         = "s3remote081292" # change this
     key            = "terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
